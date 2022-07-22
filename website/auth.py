a1d19618870bdf32a48cea5edb5b63c942450bb1
Bernard Ross <bernardross0763@gmail.com>
2022-07-21 20:59:31 -0400

    added a login required decorator

--- website/auth.py
+++ website/auth.py
@@ -3,7 +3,9 @@ from website import oauth
 from os import environ as env
 from urllib.parse import quote_plus, urlencode
 
+
 auth = Blueprint('auth', __name__)
+
 
 @auth.route('/login')
 def login():
@@ -11,11 +13,13 @@ def login():
         redirect_uri=url_for('auth.callback', _external=True)
     )
 
+
 @auth.route('/callback', methods=['GET', 'POST'])
 def callback():
 	token = oauth.auth0.authorize_access_token()
 	session["user"] = token
-	return redirect("/")
+	return redirect("/home")
+
 
 @auth.route('/logout')
 def logout():
@@ -25,7 +29,7 @@ def logout():
         + "/v2/logout?"
         + urlencode(
             {
-                "returnTo": url_for("views.home", _external=True),
+                "returnTo": url_for("views.guest", _external=True),
                 "client_id": env.get("AUTH0_CLIENT_ID"),
             },
             quote_via=quote_plus,
--- website/views.py
+++ website/views.py
@@ -1,12 +1,30 @@
-from flask import Blueprint, render_template, session
+from flask import Blueprint, render_template, session, redirect, url_for
 import json
 
 views = Blueprint('views', __name__)
 
+
+# Decorator to determine if user is logged in via session cookie
+# if not, redirect to guest
+def loggedin_required(function):
+	def wrapper(*args, **kwargs):
+		if 'user' in session:
+			return function()
+		else:
+			return redirect(url_for('views.guest'))
+
+	return wrapper
+
+
+# Guest welcome page
 @views.route('/')
+def guest():
+	return render_template('guest.html', user=None)
+
+
+# logged in home page
+@views.route('/home')
+@loggedin_required
 def home():
 	user = session.get('user')
-	if not user:
-		return render_template('guest.html', user=user)
-
 	return render_template('home.html', user=user)
