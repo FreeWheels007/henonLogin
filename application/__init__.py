dce22cb962335bf8d911696235518d718979f6b5
Bernard Ross <108906798+FreeWheels007@users.noreply.github.com>
2022-07-24 08:57:27 -0400

    Update __init__.py with comments

--- application/__init__.py
+++ application/__init__.py
@@ -3,15 +3,20 @@ from authlib.integrations.flask_client import OAuth
 from dotenv import find_dotenv, load_dotenv
 from flask import Flask
 
+# load private auth0 parameters from hidden .env file
+# NOT on Github
 ENV_FILE = find_dotenv()
 if ENV_FILE:
 	load_dotenv(ENV_FILE)
 
+# create flask app instance and link to secret key to use encrypted session data
 application = Flask(__name__)
 application.secret_key = env.get('APP_SECRET_KEY')
 
+# create OAuth instance
 oauth = OAuth(application)
 
+# link OAuth instance with auth0 account parameters
 oauth.register(
     "auth0",
     client_id=env.get("AUTH0_CLIENT_ID"),
@@ -22,6 +27,7 @@ oauth.register(
     server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
 )
 
+# load and link website routes to app
 from .views import views
 from .auth import auth
 
